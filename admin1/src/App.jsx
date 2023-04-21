2b220f582cfa8f09fe0a5553195fbe81eb650625
phamduckiet <duckietpham@gmail.com>
2023-04-21 22:11:51 +0700

    change

--- admin1/src/App.jsx
+++ admin1/src/App.jsx
@@ -1,14 +1,10 @@
-import { Space } from "antd";
-import "./App.css";
-import AppFooter from "./Components/AppFooter";
-import AppHeader from "./Components/AppHeader";
-import PageContent from "./Components/PageContent";
-import SideMenu from "./Components/SideMenu";
+
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import Dangky from "./Components/account/Dangky";
 import Login from "./Components/account/Login";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
+
 function App() {
   const selector = useSelector(state => state.userReducer);
   const navigate = useNavigate();
@@ -33,14 +29,7 @@ function App() {
         <Route path="/admin/login" element={<Login />} />
       </Route>
     </Routes>
-      <div className="App">
-        <AppHeader />
-        <div className="SideMenuAndPageContent">
-          <SideMenu></SideMenu>
-          <PageContent></PageContent>
-        </div>
-        <AppFooter />
-      </div>
+      
     </>
   );
 }
